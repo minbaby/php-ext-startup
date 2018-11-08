c47f2a859ee3781ce3ce96cf0a08d921b6364d07
minbaby.zhang <zhangshaomin_1990@126.com>
2018-11-08 22:03:45 +0800

    先提交一波吧

--- tools/build.sh
+++ tools/build.sh
@@ -7,10 +7,10 @@ pwd
 # rm -rf `phpbrew path ext/startup.so`
 
 echo "disabled startup"
-phpbrew ext disable startup
+# phpbrew ext disable startup
 
 echo "go..."
 phpize && ./configure && make && make install && phpize --clean
 
 echo "enabled startup"
-phpbrew ext enable startup
+# phpbrew ext enable startup

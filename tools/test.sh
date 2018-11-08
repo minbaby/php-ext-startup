6aad0c5b1f6abe5f5f62c6a5a98508b99a2edb52
minbaby.zhang <zhangshaomin_1990@126.com>
2018-11-08 22:07:12 +0800

    circle来一波

--- tools/test.sh
+++ tools/test.sh
@@ -2,13 +2,13 @@
 
 pwd
 
-echo "disable ext startup.so"
-phpbrew ext disable startup
+# echo "disable ext startup.so"
+# phpbrew ext disable startup
 ./vendor/bin/kahlan
 
-echo "enable ext start.so"
-phpbrew ext enable startup
-./vendor/bin/kahlan
+# echo "enable ext start.so"
+# phpbrew ext enable startup
+# ./vendor/bin/kahlan
 
 
 

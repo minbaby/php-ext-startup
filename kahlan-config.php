3cc01bdb441ace50277faad9416bbc9655b98bb9
霸气千秋 <zhangshaomin_1990@126.com>
2018-12-13 00:07:36 +0800

    do code coverage something (#24)

--- kahlan-config.php
+++ kahlan-config.php
@@ -3,5 +3,5 @@ use Kahlan\Cli\CommandLine;
 
 /**@var \Kahlan\Cli\CommandLine $commandLine */
 $commandLine = $this->commandLine();
-$commandLine->option('reporter', 'default', 'verbose');
+// $commandLine->option('reporter', 'default', 'verbose');
 $commandLine->option('ff', 'default', 1);
--- scripts/test.sh
+++ scripts/test.sh
@@ -9,10 +9,9 @@ pwd
 phpbrew ext disable startup
 export MINBABY_TEST_EXT=0
 ./vendor/bin/kahlan
+./vendor/bin/kahlan --coverage=0 --clover=coverage.xml
 
 # echo "enable ext start.so"
 phpbrew ext enable startup
 export MINBABY_TEST_EXT=1
 ./vendor/bin/kahlan
-
-

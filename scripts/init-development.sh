87537aab7a43c51a533202c01ec72b9828540497
minbaby.zhang <minbaby.zhang@gmail.com>
2019-01-13 13:27:42 +0800

    [skip ci] 更显安装脚本

--- scripts/init-development.sh
+++ scripts/init-development.sh
@@ -11,7 +11,16 @@ export SYSTEM_NAME=`uname`
 [ "$SYSTEM_NAME" == "Darwin" ] && LIB_BZ2='+bz2=/usr/local/opt/bzip2/'
 [ "$SYSTEM_NAME" == "Darwin" ] && LIB_ZIP='+zlib=/usr/local/opt/zlib'
 
-phpbrew --debug install --mirror=http://cn2.php.net -j 4 $PHP_VERSION +default $LIB_BZ2 $LIB_ZIP +mb +openssl=/usr/local/opt/openssl/
+# sudo apt install libxml2-dev
+# sudo apt install libssh-dev
+# sudo apt install libbz2-dev
+# sudo apt install libcurl4-openssl-dev
+# sudo apt install libmcrypt-dev
+# sudo apt install libreadline-dev
+# sudo apt install libxslt1-dev 
+# sudo apt install autoconf
+
+phpbrew --debug install --mirror=http://cn2.php.net -j 4 $PHP_VERSION +default $LIB_BZ2 $LIB_ZIP +mb # +openssl=/usr/local/opt/openssl/
 
 echo $PHP_VERSION
 
